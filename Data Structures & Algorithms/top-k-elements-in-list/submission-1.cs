d8b1e23ba8fedc924e4835d681c943657d3478e5
Joon Park <70257506+Auclown@users.noreply.github.com>
2026-07-27 20:14:35 +1200

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public int[] TopKFrequent(int[] nums, int k) {
+        if (nums.Length == 0 || k == 0)
+        {
+            return new int[] {};
+        }
+
+        int[] result = new int[] {};
+        Dictionary<int, int> numDict = new Dictionary<int, int>();
+
+        foreach (int num in nums)
+        {
+            if (!numDict.ContainsKey(num))
+            {
+                numDict.Add(num, 1);
+            }
+            else
+            {
+                numDict[num] += 1;
+            }
+        }
+
+        return numDict
+            .OrderByDescending(kvp => kvp.Value)
+            .Select(kvp => kvp.Key)
+            .Take(k)
+            .ToArray();
+    }
+}
