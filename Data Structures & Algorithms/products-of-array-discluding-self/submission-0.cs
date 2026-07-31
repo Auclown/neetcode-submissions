87b412c853d63209e28062c8198fd393ccfafb1f
Joon Park <70257506+Auclown@users.noreply.github.com>
2026-07-31 20:01:33 +1200

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        int n = nums.Length;
+        int[] result = new int[n];
+
+        // Pass 1: Calculate prefix products (products of elements to the left)
+        int prefix = 1;
+        for (int i = 0; i < n; i++) 
+        {
+            result[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        // Pass 2: Multiply by suffix products (products of elements to the right)
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) 
+        {
+            result[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return result;
+    }
+}
